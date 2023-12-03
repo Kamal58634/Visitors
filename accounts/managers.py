68ac70c3c6f07135f9725e29a6251e93c481e978
kamal gharibi <kamal58634@gmail.com>
2023-12-03 12:33:13 -0800

    Add costum manager for User Model

---
+++ accounts/managers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import BaseUserManager
+from .models import User
+
+class UserManager(BaseUserManager):
+    def create_user(self,phone_number,email,full_name,password=None):
+        if not email:
+            raise ValueError("user must be have email")
+        if not phone_number:
+            raise ValueError("user must be have email")
+        if not full_name:
+            raise ValueError("user must be have email")
+        user1=User(phone_number=phone_number,full_name=full_name,
+                        email=self.normalize_email(email))
+        user1.set_password(password)
+        user1.save(using=self._db)
+        user=self.model(phone_number=phone_number,full_name=full_name,
+                        email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,phone_number,email,full_name,password=None):
+        user=self.create_user(phone_number,email,full_name,password)
+        user.is_admin=True
+        user.save(using=self._db)
+        return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-from 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
+
+
+class User(AbstractBaseUser):
+    email=models.EmailField(max_length=200,unique=True)
+    full_name=models.CharField(max_length=200)
+    phone_number=models.CharField(max_length=11,unique=True)
+    is_active=models.BooleanField(default=True)
+    is_admin=models.BooleanField(default=False)
+
+    objects=UserManager()
+    USERNAME_Field='phone_number'
+    REQUIRED_FIELDS =('email','full_name')
+
+
+    def __str__(self):
+        return f'{self.full_name}-{self.phone_number}'
+    
+    def has_perm(self,perm,obj=None):
+        return True
+    
+    def has_module_perms(self,app_labble):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
